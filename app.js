16d0f6e72dc1a4f0db44a97256f8570e517de8a3
harshavardan7 <17harshavardan@gmail.com>
2025-06-25 10:24:09 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,16 @@
+//https://media.istockphoto.com/id/679896474/vector/light-bulb-glow-yellow-color.webp?s=612x612&w=is&k=20&c=bPF4E8iTvGz-uYVCUN9slu74Y7fpbf9GVpKWh_JewL4=
+
+
+//https://media.istockphoto.com/id/153754853/photo/screw-lightbulb-photo-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=89C5tXFQ6O1G7nMlY--bk6klLlJzs3bli8DrauB_XEk=
+
+const on_btn=document.getElementById("on");
+const off_btn=document.getElementById("off");
+var img=document.getElementById("bulb");
+
+function bulb_on(){
+    img.src="https://media.istockphoto.com/id/679896474/vector/light-bulb-glow-yellow-color.webp?s=612x612&w=is&k=20&c=bPF4E8iTvGz-uYVCUN9slu74Y7fpbf9GVpKWh_JewL4="
+}
+
+function bulb_off(){
+    img.src="https://media.istockphoto.com/id/153754853/photo/screw-lightbulb-photo-isolated-on-white-background.jpg?s=612x612&w=0&k=20&c=89C5tXFQ6O1G7nMlY--bk6klLlJzs3bli8DrauB_XEk="
+}
